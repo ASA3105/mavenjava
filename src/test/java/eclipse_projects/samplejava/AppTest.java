0bb56998e45ab21e4ed4dabae47857a229d1cb66
unknown <21bd1a0541cse@gmail.com>
2024-01-30 22:01:28 +0530

    mavenjava push

---
+++ src/test/java/eclipse_projects/samplejava/AppTest.java
@@ -0,0 +1,20 @@
+package eclipse_projects.samplejava;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
